8d8b5ba17d3dd57505e49f76f45749005d5c232d
Urban Pettersson <urbanpe88@gmail.com>
2020-09-13 19:11:41 +0200

    chore: add (questionable) env file

---
+++ src/helpers/env.ts
@@ -0,0 +1,10 @@
+// istanbul ignore file
+
+import { Variables } from '@models/env'
+
+/**
+ * Static list of env variables
+ */
+export const variables: Variables = {
+  POEditorBaseUrl: 'https://api.poeditor.com/v2'
+}
